46f245229a719ccb3947305f33d4afef01edf87c
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-08-09 19:41:26 +0300

    create new component SectionWithIcon for Education and Experience

--- src/widgets/aside/Aside.tsx
+++ src/widgets/aside/Aside.tsx
@@ -5,6 +5,9 @@ import {faCalendar, faEnvelope, faLocationDot, faPhone, } from "@fortawesome/fre
 import {faGithub, } from "@fortawesome/free-brands-svg-icons";
 import ContactsItem  from "../../features/contactsItem/ContactsItem.tsx";
 import List from "../../shared/list/List.tsx";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import {faXTwitter} from "@fortawesome/free-brands-svg-icons/faXTwitter";
+import {faTelegram} from "@fortawesome/free-brands-svg-icons/faTelegram";
 
 const Aside = () => {
     // TODO: need add to project My image and use for img
@@ -36,9 +39,18 @@ const Aside = () => {
 
             <div className="aside__social__media">
                 <a href="https://github.com/Observersss" className="aside__social__media__link">
-                    <FontAwesomeIcon icon={faGithub}  size="xl"/>
+                    <FontAwesomeIcon icon={faGithub} size="xl"/>
                 </a>
-            {/*    TODO: need add links : LinkedIn, X,telegram*/}
+                <a href="https://www.linkedin.com/in/illia-honcharenko-16477826b/"
+                   className="aside__social__media__link">
+                    <FontAwesomeIcon icon={faLinkedin} size="xl"/>
+                </a>
+                <a href="https://x.com/Touch78216070" className="aside__social__media__link">
+                    <FontAwesomeIcon icon={faXTwitter} size="xl"/>
+                </a>
+                <a href="https://t.me/dream_bury_me" className="aside__social__media__link">
+                    <FontAwesomeIcon icon={faTelegram} size="xl"/>
+                </a>
             </div>
         </aside>
     )
