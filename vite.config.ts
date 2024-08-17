3ee925335585901ee4298904c0f6420a74878dc3
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-08-17 22:42:11 +0300

    fix... version +100500+4

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/portfolio/",
-  base: '',
+  base: "/portfolio/",
   server: {
     // Это может помочь в отладке
     open: true,
