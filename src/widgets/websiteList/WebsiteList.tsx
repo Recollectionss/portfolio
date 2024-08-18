7f5614836022b074e6188b497aa0cdd3ff035ddc
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-08-18 18:01:15 +0300

    add normal link to Website Posts and img

--- src/widgets/websiteList/WebsiteList.tsx
+++ src/widgets/websiteList/WebsiteList.tsx
@@ -1,10 +1,11 @@
 import classes from "./_websiteList.module.scss";
 import WebsiteItem from "../websiteItem/WebsiteItem.tsx";
+import Posts from '/WebsitePosts.png'
 const WebsiteList = () => {
 
     const websites = [
         {img:'',href:"https://github.com/Observersss/Netflix-clone",title:"Netflix clone", alt: "Clone"},
-        {img:'',href:"",title:`Post (use JSONPLACEHOLDER)`, alt: "Website post"},
+        {img:Posts,href:"https://observersss.github.io/test_react/",title:`Post (use JSONPLACEHOLDER)`, alt: "Website post"},
         {img:'',href:"",title:"To-Do list", alt: "Website todos"},
 
     ];
