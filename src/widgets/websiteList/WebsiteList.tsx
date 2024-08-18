3b6c16b670aa6bff91c210b8cf3f857d82c10d36
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-08-18 18:16:31 +0300

    add normal link to Website Todos and img

--- src/widgets/websiteList/WebsiteList.tsx
+++ src/widgets/websiteList/WebsiteList.tsx
@@ -1,12 +1,13 @@
 import classes from "./_websiteList.module.scss";
 import WebsiteItem from "../websiteItem/WebsiteItem.tsx";
 import Posts from '/WebsitePosts.png'
+import ToDos from '/WebsiteToDo.png'
 const WebsiteList = () => {
 
     const websites = [
         {img:'',href:"https://github.com/Observersss/Netflix-clone",title:"Netflix clone", alt: "Clone"},
         {img:Posts,href:"https://observersss.github.io/test_react/",title:`Post (use JSONPLACEHOLDER)`, alt: "Website post"},
-        {img:'',href:"",title:"To-Do list", alt: "Website todos"},
+        {img: ToDos,href:"https://observersss.github.io/to-do-list/",title:"To-Do list", alt: "Website todos"},
 
     ];
 
