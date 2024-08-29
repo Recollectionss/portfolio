2e2005f9a3b1a8c05352c97b69671fe92a75a51b
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-08-29 20:42:56 +0300

    months...

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -9,7 +9,7 @@ const Resume = () => {
         {title: "Taras Shevchenko National University of Kyiv", dates: "2022 - Present", info: "121 specialization - Software Engineer"},
     ];
     const itemsExperience:SectionItem[] = [
-        {title: "C++", dates: "1-3 moth", info:"I worked on a training project for several months as part of a two-person team. My responsibilities included developing the gamified portion of the application and creating tasks for our mini-team to streamline the development process using C++ and Qt."},
+        {title: "C++", dates: "1-3 months", info:"I worked on a training project for several months as part of a two-person team. My responsibilities included developing the gamified portion of the application and creating tasks for our mini-team to streamline the development process using C++ and Qt."},
     ];
     const itemsStack:SectionItem[] = [
         {title: "HTML"},
