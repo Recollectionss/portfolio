2d6a0589c62bc23cf741c59fb8bf85a30f978f29
Observersss <heavyobjectics@gmail.com>
2025-01-30 17:47:57 +0200

    some fix

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -11,7 +11,7 @@ const Resume = () => {
     ];
     const itemsExperience:SectionItem[] = [
         {title: "Confero (Discord Voting Bot, Node.js and TypeScript)", dates: "1 months", info:"Fully functional voting bot for Discord tailored for CS FSCS to digitalize voting processes. The bot leverages database migrations, middleware, and transactions to ensure reliability and efficiency. It follows a modular architecture, allowing for easy scalability and future feature expansion."},
-        {title: "Blue Ship Sea Battle, Flutter)", dates: "2 weeks", info:" Battleship game app using Flutter, featuring an interactive interface and smooth gameplay. The app offers a seamless gaming experience and demonstrates effective use of Flutter's cross-platform capabilities."},
+        {title: "Blue Ship (Sea Battle, Flutter)", dates: "2 weeks", info:" Battleship game app using Flutter, featuring an interactive interface and smooth gameplay. The app offers a seamless gaming experience and demonstrates effective use of Flutter's cross-platform capabilities."},
         {title: "Silently (To-do app with RPG system C++)", dates: "3 months", info:"I worked on a training project for several months as part of a two-person team. My responsibilities included developing the gamified portion of the application and creating tasks for our mini-team to streamline the development process using C++ and Qt."},
     ];
     const itemsStack:SectionItem[] = [
