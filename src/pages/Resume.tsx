0f1a9e065bbbd7fcfa8664e6d8d55d8d219d52e3
Observersss <heavyobjectics@gmail.com>
2025-01-30 17:46:58 +0200

    add more info about projects

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -10,7 +10,9 @@ const Resume = () => {
         {title: "Database Structures and Management with MySQL", dates: "May 2024"},
     ];
     const itemsExperience:SectionItem[] = [
-        {title: "C++", dates: "1-3 months", info:"I worked on a training project for several months as part of a two-person team. My responsibilities included developing the gamified portion of the application and creating tasks for our mini-team to streamline the development process using C++ and Qt."},
+        {title: "Confero (Discord Voting Bot, Node.js and TypeScript)", dates: "1 months", info:"Fully functional voting bot for Discord tailored for CS FSCS to digitalize voting processes. The bot leverages database migrations, middleware, and transactions to ensure reliability and efficiency. It follows a modular architecture, allowing for easy scalability and future feature expansion."},
+        {title: "Blue Ship Sea Battle, Flutter)", dates: "2 weeks", info:" Battleship game app using Flutter, featuring an interactive interface and smooth gameplay. The app offers a seamless gaming experience and demonstrates effective use of Flutter's cross-platform capabilities."},
+        {title: "Silently (To-do app with RPG system C++)", dates: "3 months", info:"I worked on a training project for several months as part of a two-person team. My responsibilities included developing the gamified portion of the application and creating tasks for our mini-team to streamline the development process using C++ and Qt."},
     ];
     const itemsStack:SectionItem[] = [
         {title: "JavaScript / TypeScript"},
